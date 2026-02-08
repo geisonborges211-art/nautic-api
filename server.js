197ab105dc95edc5f91a22ce480e91b1b017138f
geisonborges211-art <geisonborges211@gmail.com>
2026-02-07 23:18:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 import express from "express";
 
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ status: "API funcionando 🚀" });
 });
 
-app.post("/gerar", (req, res) => {
-  res.json({
-    titulo: "Campanha Náutica Premium",
-    subtitulo: "Performance e confiança no mar",
-    cta: "Compre agora"
-  });
-});
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
